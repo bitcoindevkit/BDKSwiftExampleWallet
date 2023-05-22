5722c2e4bddbf6c39848f9c66a8d26067c8e4494
Matthew <ramsden@hey.com>
2023-05-22 10:12:40 -0500

    first commit

---
+++ BDKSwiftExampleWallet/BDKSwiftExampleWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  BDKSwiftExampleWalletApp.swift
+//  BDKSwiftExampleWallet
+//
+//  Created by Matthew Ramsden on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct BDKSwiftExampleWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
