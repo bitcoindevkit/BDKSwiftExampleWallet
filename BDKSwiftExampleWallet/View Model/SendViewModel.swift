a8ff43f5cd1219fd5a15d756efb8e0a3035769b5
Matthew <6657488+reez@users.noreply.github.com>
2023-08-23 15:09:37 -0500

    ui: fees

--- BDKSwiftExampleWallet/View Model/SendViewModel.swift
+++ BDKSwiftExampleWallet/View Model/SendViewModel.swift
@@ -12,7 +12,62 @@ import Observation
 @MainActor
 @Observable
 class SendViewModel {
+    let feeService: FeeService
     var balanceTotal: UInt64 = 0
+    var recommendedFees: RecommendedFees?
+    var selectedFeeIndex: Int = 2
+    var selectedFee: Int? {
+        guard let fees = recommendedFees else {
+            return nil
+        }
+        switch selectedFeeIndex {
+        case 0: return fees.minimumFee
+        case 1: return fees.hourFee
+        case 2: return fees.halfHourFee
+        default: return fees.fastestFee
+        }
+    }
+    
+    var selectedFeeDescription: String {
+        guard let selectedFee = selectedFee else {
+            return "Failed to load fees"
+        }
+        
+        let feeText = text(for: selectedFeeIndex)
+        return "Selected \(feeText) Fee: \(selectedFee) sats"
+    }
+    
+    private func text(for index: Int) -> String {
+        
+        switch index {
+            
+        //"Minimum Fee"
+        case 0:
+            return "No Priority"
+            
+        //"Hour Fee"
+        case 1:
+            return "Low Priority"
+            
+        //"Half Hour Fee"
+        case 2:
+            return "Medium Priority"
+            
+        //"Fastest Fee"
+        case 3:
+            return "High Priority"
+            
+        default:
+            return ""
+            
+        }
+        
+    }
+    
+    init(feeService: FeeService) {
+        self.feeService = feeService
+    }
+    
     
     func getBalance() {
         do {
@@ -29,9 +84,18 @@ class SendViewModel {
         do {
             try BDKService.shared.send(address: address, amount: amount, feeRate: feeRate)
         } catch let error as WalletError {
-            print("getBalance - Wallet Error: \(error.localizedDescription)")
+            print("send - Wallet Error: \(error.localizedDescription)")
         } catch {
-            print("getBalance - Undefined Error: \(error.localizedDescription)")
+            print("send - Undefined Error: \(error.localizedDescription)")
+        }
+    }
+    
+    func getFees() async {
+        do {
+            let recommendedFees = try await feeService.fees()
+            self.recommendedFees = recommendedFees
+        } catch {
+            print("getFees error: \(error.localizedDescription)")
         }
     }
     
