23c06736ba090b38a78ccf8def633be74ae247d1
Matthew <ramsden@hey.com>
2023-09-21 12:33:39 -0500

    add mempool testnet

--- BDKSwiftExampleWallet/Utilities/Constants.swift
+++ BDKSwiftExampleWallet/Utilities/Constants.swift
@@ -11,10 +11,13 @@ struct Constants {
     struct Config {
         struct EsploraServerURLNetwork {
             struct Signet {
-                static let mutiny = "https://mutinynet.com/api"
                 static let bdk = "http://signet.bitcoindevkit.net:3003/"
+                static let mutiny = "https://mutinynet.com/api"
             }
-            static let testnet = "http://blockstream.info/testnet/api/"
+            struct Testnet {
+                static let blockstream = "http://blockstream.info/testnet/api/"
+                static let mempool = "https://mempool.space/testnet/api/"
+            }
         }
     }
 }
