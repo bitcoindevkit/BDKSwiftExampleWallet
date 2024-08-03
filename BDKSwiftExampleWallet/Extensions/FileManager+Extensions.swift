6eb8f971db2bae84914cca3628d825b5ac32c584
Matthew <ramsden@hey.com>
2024-08-03 10:46:30 -0500

    format

--- BDKSwiftExampleWallet/Extensions/FileManager+Extensions.swift
+++ BDKSwiftExampleWallet/Extensions/FileManager+Extensions.swift
@@ -20,27 +20,27 @@ extension FileManager {
             try removeItem(at: fileURL)
         }
     }
-    
-    func ensureDirectoryExists(at url: URL) throws {
-         var isDir: ObjCBool = false
-         if fileExists(atPath: url.path, isDirectory: &isDir) {
-             if !isDir.boolValue {
-                 try removeItem(at: url)
-             }
-         }
-         if !fileExists(atPath: url.path) {
-             try createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
-         }
-     }
 
-     func removeOldFlatFileIfNeeded(at directoryURL: URL) throws {
-         let flatFileURL = directoryURL.appendingPathComponent("wallet_data")
-         var isDir: ObjCBool = false
-         if fileExists(atPath: flatFileURL.path, isDirectory: &isDir) {
-             if !isDir.boolValue {
-                 try removeItem(at: flatFileURL)
-             }
-         }
-     }
-    
+    func ensureDirectoryExists(at url: URL) throws {
+        var isDir: ObjCBool = false
+        if fileExists(atPath: url.path, isDirectory: &isDir) {
+            if !isDir.boolValue {
+                try removeItem(at: url)
+            }
+        }
+        if !fileExists(atPath: url.path) {
+            try createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+        }
+    }
+
+    func removeOldFlatFileIfNeeded(at directoryURL: URL) throws {
+        let flatFileURL = directoryURL.appendingPathComponent("wallet_data")
+        var isDir: ObjCBool = false
+        if fileExists(atPath: flatFileURL.path, isDirectory: &isDir) {
+            if !isDir.boolValue {
+                try removeItem(at: flatFileURL)
+            }
+        }
+    }
+
 }
