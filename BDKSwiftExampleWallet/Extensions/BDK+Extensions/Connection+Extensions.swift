5302fe32c8295079470d94973d1e2c50e4b01552
Rubens <rubens.machion@gmail.com>
2025-05-17 12:30:06 -0300

    fix: create connection crash

--- BDKSwiftExampleWallet/Extensions/BDK+Extensions/Connection+Extensions.swift
+++ BDKSwiftExampleWallet/Extensions/BDK+Extensions/Connection+Extensions.swift
@@ -3,12 +3,8 @@ import Foundation
 
 extension Connection {
     static func createConnection() throws -> Connection {
-        let documentsDirectoryURL = URL.documentsDirectory
+        let documentsDirectoryURL = URL.defaultWalletDirectory
         let walletDataDirectoryURL = documentsDirectoryURL.appendingPathComponent(URL.walletDirectoryName)
-
-        if FileManager.default.fileExists(atPath: walletDataDirectoryURL.path) {
-            try FileManager.default.removeItem(at: walletDataDirectoryURL)
-        }
 
         try FileManager.default.ensureDirectoryExists(at: walletDataDirectoryURL)
         try FileManager.default.removeOldFlatFileIfNeeded(at: documentsDirectoryURL)
