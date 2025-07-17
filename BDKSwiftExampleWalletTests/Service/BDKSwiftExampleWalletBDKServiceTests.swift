e2f95b2603fa249affe97076e9bc806e64a31071
Matthew <ramsden@hey.com>
2025-07-17 13:35:13 -0500

    fix testBDKClientMockGetTransactions

--- BDKSwiftExampleWalletTests/Service/BDKSwiftExampleWalletBDKServiceTests.swift
+++ BDKSwiftExampleWalletTests/Service/BDKSwiftExampleWalletBDKServiceTests.swift
@@ -29,7 +29,7 @@ final class BDKSwiftExampleWalletBDKServiceTests: XCTestCase {
 
         XCTAssertEqual(
             transactionDetails.first?.transaction.transactionID,
-            Transaction.mock?.computeTxid()
+            Transaction.mock?.transactionID
         )
     }
 
